c44aaa382f085aa705cf501251ea2b0bc572addd
Graham King <animandosolutions@gmail.com>
2023-01-07 18:37:14 +0000

    Configure tf state

--- main.tf
+++ main.tf
@@ -6,6 +6,15 @@ terraform {
     }
   }
 
+  backend "http" {
+    address        = "https://api.tfstate.dev/github/v1"
+    lock_address   = "https://api.tfstate.dev/github/v1/lock"
+    unlock_address = "https://api.tfstate.dev/github/v1/lock"
+    lock_method    = "PUT"
+    unlock_method  = "DELETE"
+    username       = "animando/catalyst-infra"
+  }
+
   required_version = ">= 1.2.0"
 }
 
