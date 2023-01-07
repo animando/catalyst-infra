51ea53b9e0886c9b36aa4edee88f14b7ce36513f
Graham King <animandosolutions@gmail.com>
2023-01-07 16:50:13 +0000

    Fix ami id

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
+  ami           = "ami-084e8c05825742534"
   instance_type = "t2.micro"
 
   tags = {
