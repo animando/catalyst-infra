06f36e6b9f971deaef9a5301c8f33a5db29c5e94
Graham King <animandosolutions@gmail.com>
2023-01-07 20:28:18 +0000

    Allow ssh ingress

--- main.tf
+++ main.tf
@@ -30,8 +30,7 @@ resource "aws_security_group" "ec2_sg" {
     from_port        = 22
     to_port          = 22
     protocol         = "tcp"
-    cidr_blocks      = [aws_vpc.main.cidr_block]
-    ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
+    cidr_blocks      = ["0.0.0.0/0"]
   }
 
   egress {
@@ -39,7 +38,6 @@ resource "aws_security_group" "ec2_sg" {
     to_port          = 0
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
   }
 
   tags = {
