be2470db9f8ea9f95d5e3bf4efadc9e30229f365
Graham King <animandosolutions@gmail.com>
2023-01-09 09:19:29 +0000

    Configure cloudfront s3 access

--- cloudfront.tf
+++ cloudfront.tf
@@ -16,6 +16,8 @@ resource "aws_cloudfront_origin_access_identity" "ui_cloudfront_oai" {
 
 resource "aws_cloudfront_distribution" "ui_cloudfront_distribution" {
   origin {
+    domain_name              = aws_s3_bucket.ui_website_bucket.bucket_regional_domain_name
+    origin_id                = local.s3_origin_id
     s3_origin_config {
       origin_access_identity = aws_cloudfront_origin_access_identity.ui_cloudfront_oai
     }
