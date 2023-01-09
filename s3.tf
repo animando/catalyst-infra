10da2eb1a34df4f521ff5a846d8ca42c30407c17
Graham King <animandosolutions@gmail.com>
2023-01-09 08:53:51 +0000

    Block public access on test bucket

--- s3.tf
+++ s3.tf
@@ -2,6 +2,15 @@ resource "aws_s3_bucket" "test_bucket" {
   bucket = "animando-catalyst-test-bucket"
 }
 
+resource "aws_s3_bucket_public_access_block" "test_bucket_public_block" {
+  bucket = aws_s3_bucket.test_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
 resource "aws_s3_bucket_website_configuration" "test_bucket_configuration" {
   bucket = aws_s3_bucket.test_bucket.bucket
 
