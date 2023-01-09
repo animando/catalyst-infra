cc3f5300ad8acc305839a6f346b0a8789a580a53
Graham King <animandosolutions@gmail.com>
2023-01-09 08:48:16 +0000

    Remove policy for test bucket

--- s3.tf
+++ s3.tf
@@ -2,31 +2,6 @@ resource "aws_s3_bucket" "test_bucket" {
   bucket = "animando-catalyst-test-bucket"
 }
 
-resource "aws_s3_bucket_policy" "test_bucket_policy" {
-  bucket = aws_s3_bucket.test_bucket.id
-  policy = data.aws_iam_policy_document.test_bucket_policy.json
-}
-
-data "aws_iam_policy_document" "test_bucket_policy" {
-  statement {
-
-    principals {
-      type        = "AWS"
-      identifiers = [
-        "*"
-      ]
-    }
-    actions = [
-      "s3:GetObject"
-    ]
-
-    resources = [
-      aws_s3_bucket.test_bucket.arn,
-      "${aws_s3_bucket.test_bucket.arn}/*",
-    ]
-  }
-}
-
 resource "aws_s3_bucket_website_configuration" "test_bucket_configuration" {
   bucket = aws_s3_bucket.test_bucket.bucket
 
