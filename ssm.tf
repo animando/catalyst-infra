1ecf10591842cf4d7dd72bc56f80dd13ff07eb60
Graham King <animandosolutions@gmail.com>
2023-01-10 21:04:49 +0000

    Fix msk ssm

--- ssm.tf
+++ ssm.tf
@@ -4,17 +4,17 @@ resource "aws_ssm_parameter" "ui_distribution_domain" {
   value = aws_cloudfront_distribution.ui_cloudfront_distribution.domain_name
 }
 
-resource "aws_ssm_parameter" "kafka_broker_connect_string" {
-  name  = "catalyst-kafka-broker-connect-string"
-  type  = "String"
-  value = aws_msk_cluster.msk_cluster.bootstrap_brokers
-}
-
-# resource "aws_ssm_parameter" "kafka_cluster_id" {
-#   name  = "catalyst-kafka-cluster-id"
+# resource "aws_ssm_parameter" "kafka_broker_connect_string" {
+#   name  = "catalyst-kafka-broker-connect-string"
 #   type  = "String"
-#   value = aws_msk_cluster.msk_cluster.id
+#   value = aws_msk_cluster.msk_cluster.bootstrap_brokers
 # }
+
+resource "aws_ssm_parameter" "kafka_cluster_id" {
+  name  = "catalyst-kafka-cluster-id"
+  type  = "String"
+  value = aws_msk_cluster.msk_cluster.id
+}
 
 # resource "aws_ssm_parameter" "kafka_bootstrap_brokers_iam" {
 #   name  = "catalyst-kafka-bootstrap-brokers-iam"
