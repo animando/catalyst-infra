4e2e057e7e2ae8af98166d26202dfa7d889d058a
Graham King <animandosolutions@gmail.com>
2023-01-21 16:37:19 +0000

    private vpc route out

--- vpc.tf
+++ vpc.tf
@@ -135,11 +135,9 @@ resource "aws_vpc_endpoint" "dynamodb_vpc_endpoint" {
 
   route_table_ids = [
     aws_vpc.vpc.default_route_table_id,
+    aws_route_table.private_route_table.id,
+    aws_route_table.private_route_table.id,
     aws_route_table.private_route_table.id
-  ]
-
-  security_group_ids = [
-    aws_security_group.allow_vpc_https_traffic.id
   ]
 
   private_dns_enabled = true
@@ -216,14 +214,33 @@ resource "aws_vpc_endpoint" "secretsmanager_vpc_endpoint" {
 }
 
 #
-
-resource "aws_eip" "eip" {
+resource "aws_eip" "eip_az1" {
   vpc = true
+
+  tags = {
+    Project = "Catalyst"
+  }
+}
+
+resource "aws_eip" "eip_az2" {
+  vpc = true
+
+  tags = {
+    Project = "Catalyst"
+  }
+}
+
+resource "aws_eip" "eip_az3" {
+  vpc = true
+
+  tags = {
+    Project = "Catalyst"
+  }
 }
 
 
 resource "aws_nat_gateway" "public_nat_gateway_az1" {
-  allocation_id = aws_eip.eip.id
+  allocation_id = aws_eip.eip_az1.id
   subnet_id = aws_subnet.public_subnet_az1.id
 
   tags = {
@@ -236,7 +253,7 @@ resource "aws_nat_gateway" "public_nat_gateway_az1" {
 }
 
 resource "aws_nat_gateway" "public_nat_gateway_az2" {
-  allocation_id = aws_eip.eip.id
+  allocation_id = aws_eip.eip_az2.id
   subnet_id = aws_subnet.public_subnet_az2.id
 
   tags = {
@@ -248,9 +265,9 @@ resource "aws_nat_gateway" "public_nat_gateway_az2" {
   ]
 }
 
-resource "aws_nat_gateway" "public_nat_gateway_az2" {
-  allocation_id = aws_eip.eip.id
-  subnet_id = aws_subnet.public_subnet_az2.id
+resource "aws_nat_gateway" "public_nat_gateway_az3" {
+  allocation_id = aws_eip.eip_az3.id
+  subnet_id = aws_subnet.public_subnet_az3.id
 
   tags = {
     Project = "Catalyst"
@@ -259,6 +276,32 @@ resource "aws_nat_gateway" "public_nat_gateway_az2" {
   depends_on = [
     aws_internet_gateway.igw
   ]
+}
+
+resource "aws_route_table" "private_route_table_az1" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.public_nat_gateway_az1.id
+  }
+
+  tags = {
+    Project = "Catalyst"
+  }
+}
+
+resource "aws_route_table" "private_route_table_az2" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.public_nat_gateway_az2.id
+  }
+
+  tags = {
+    Project = "Catalyst"
+  }
 }
 
 resource "aws_route_table" "private_route_table_az3" {
