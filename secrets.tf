2c795d5f530a12feeb89c91a2d2e4a047f4bfbee
Graham King <animandosolutions@gmail.com>
2023-01-23 12:12:37 +0000

    Rename secret

--- secrets.tf
+++ secrets.tf
@@ -1,30 +1,15 @@
-resource "aws_secretsmanager_secret" "userpool_client_secret" {
-  name = "userpool_client_secret"
+resource "aws_secretsmanager_secret" "cognito_userpool_client_secret" {
+  name = "cognito_userpool_client_secret"
+
+  recovery_window_in_days = 0
 
   tags = {
     Name = "Catalyst User Pool Client Secret"
     Project = "Catalyst"
   }
 }
-resource "aws_secretsmanager_secret" "google_idp_client_id" {
-  name = "google_idp_client_id"
 
-  tags = {
-    Name = "Google Client Id"
-    Project = "Catalyst"
-  }
-}
-
-resource "aws_secretsmanager_secret" "google_idp_client_secret" {
-  name = "google_idp_client_secret"
-
-  tags = {
-    Name = "Google Client Secret"
-    Project = "Catalyst"
-  }
-}
-
-resource "aws_secretsmanager_secret_version" "userpool_client_secret_version" {
-  secret_id     = aws_secretsmanager_secret.userpool_client_secret.id
+resource "aws_secretsmanager_secret_version" "cognito_userpool_client_secret_version" {
+  secret_id     = aws_secretsmanager_secret.cognito_userpool_client_secret.id
   secret_string = aws_cognito_user_pool_client.catalyst_cognito_client_app.client_secret
 }
