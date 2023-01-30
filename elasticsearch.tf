c5374cb85b5f4d919d8015f2a314a3ef4ac3cdfd
Graham King <animandosolutions@gmail.com>
2023-01-30 14:42:41 +0000

    Configure OpenSearch

--- elasticsearch.tf
+++ elasticsearch.tf
@@ -1,9 +1,10 @@
-resource "aws_elasticsearch_domain" "es_domain" {
-  domain_name           = "es-domain"
-  elasticsearch_version = "7.10"
+resource "aws_opensearch_domain" "os_domain" {
+  domain_name           = "os-domain"
+
+  engine_version = "OpenSearch_2.3"
 
   cluster_config {
-    instance_type = "t3.medium.elasticsearch"
+    instance_type = "t3.medium.search"
     instance_count = 1
   }
 
@@ -26,14 +27,12 @@ resource "aws_elasticsearch_domain" "es_domain" {
 
   domain_endpoint_options {
     enforce_https = true
-    tls_security_policy = "Policy-Min-TLS-1-0-2019-07"
+    tls_security_policy = "Policy-Min-TLS-1-2-2019-07"
   }
 
   ebs_options {
     ebs_enabled = true
     volume_size = 10
-    volume_type = "gp3"
-    throughput = 125
   }
 
   tags = {
