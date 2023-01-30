07e12b312240a5cb9eee622bc21979d37d7c2a2f
Graham King <animandosolutions@gmail.com>
2023-01-30 08:33:17 +0000

    Configure ES

--- elasticsearch.tf
+++ elasticsearch.tf
@@ -26,6 +26,7 @@ resource "aws_elasticsearch_domain" "es_domain" {
 
   domain_endpoint_options {
     enforce_https = true
+    tls_security_policy = "Policy-Min-TLS-1-0-2019-07"
   }
 
   tags = {
