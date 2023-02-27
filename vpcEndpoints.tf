7211cc47a92ebd600fdb48e4a40fc3027f1d9159
Graham King <animandosolutions@gmail.com>
2023-02-27 16:39:51 +0000

    Remove VPC endpoints

--- vpcEndpoints.tf
+++ vpcEndpoints.tf
@@ -1,129 +1,129 @@
-resource "aws_vpc_endpoint" "lambda_vpc_endpoint" {
-  vpc_id = aws_vpc.vpc.id
-  service_name = "com.amazonaws.eu-west-2.lambda"
-  vpc_endpoint_type = "Interface"
+# resource "aws_vpc_endpoint" "lambda_vpc_endpoint" {
+#   vpc_id = aws_vpc.vpc.id
+#   service_name = "com.amazonaws.eu-west-2.lambda"
+#   vpc_endpoint_type = "Interface"
 
-  subnet_ids = [
-    aws_subnet.subnet_az1.id,
-    aws_subnet.subnet_az2.id,
-    aws_subnet.subnet_az3.id
-  ]
+#   subnet_ids = [
+#     aws_subnet.subnet_az1.id,
+#     aws_subnet.subnet_az2.id,
+#     aws_subnet.subnet_az3.id
+#   ]
 
-  security_group_ids = [
-    aws_security_group.allow_vpc_https_traffic.id
-  ]
+#   security_group_ids = [
+#     aws_security_group.allow_vpc_https_traffic.id
+#   ]
 
-  private_dns_enabled = true
+#   private_dns_enabled = true
 
-  tags = {
-    Name = "Lambda VPC Endpoint"
-    Project = "Catalyst"
-  }
-}
+#   tags = {
+#     Name = "Lambda VPC Endpoint"
+#     Project = "Catalyst"
+#   }
+# }
 
-resource "aws_vpc_endpoint" "sns_vpc_endpoint" {
-  vpc_id = aws_vpc.vpc.id
-  service_name = "com.amazonaws.eu-west-2.sns"
-  vpc_endpoint_type = "Interface"
+# resource "aws_vpc_endpoint" "sns_vpc_endpoint" {
+#   vpc_id = aws_vpc.vpc.id
+#   service_name = "com.amazonaws.eu-west-2.sns"
+#   vpc_endpoint_type = "Interface"
 
-  subnet_ids = [
-    aws_subnet.subnet_az1.id,
-    aws_subnet.subnet_az2.id,
-    aws_subnet.subnet_az3.id
-  ]
+#   subnet_ids = [
+#     aws_subnet.subnet_az1.id,
+#     aws_subnet.subnet_az2.id,
+#     aws_subnet.subnet_az3.id
+#   ]
 
-  security_group_ids = [
-    aws_security_group.allow_vpc_https_traffic.id
-  ]
+#   security_group_ids = [
+#     aws_security_group.allow_vpc_https_traffic.id
+#   ]
 
-  private_dns_enabled = true
+#   private_dns_enabled = true
 
-  tags = {
-    Name = "SNS VPC Endpoint"
-    Project = "Catalyst"
-  }
-}
+#   tags = {
+#     Name = "SNS VPC Endpoint"
+#     Project = "Catalyst"
+#   }
+# }
 
-resource "aws_vpc_endpoint" "dynamodb_vpc_endpoint" {
-  vpc_id = aws_vpc.vpc.id
-  vpc_endpoint_type = "Gateway"
-  service_name = "com.amazonaws.eu-west-2.dynamodb"
+# resource "aws_vpc_endpoint" "dynamodb_vpc_endpoint" {
+#   vpc_id = aws_vpc.vpc.id
+#   vpc_endpoint_type = "Gateway"
+#   service_name = "com.amazonaws.eu-west-2.dynamodb"
 
-  route_table_ids = [
-    aws_vpc.vpc.default_route_table_id
-  ]
+#   route_table_ids = [
+#     aws_vpc.vpc.default_route_table_id
+#   ]
 
-  tags = {
-    Name = "DynamoDb VPC Endpoint"
-    Project = "Catalyst"
-  }
-}
+#   tags = {
+#     Name = "DynamoDb VPC Endpoint"
+#     Project = "Catalyst"
+#   }
+# }
 
-resource "aws_vpc_endpoint" "sts_vpc_endpoint" {
-  vpc_id = aws_vpc.vpc.id
-  service_name = "com.amazonaws.eu-west-2.sts"
-  vpc_endpoint_type = "Interface"
+# resource "aws_vpc_endpoint" "sts_vpc_endpoint" {
+#   vpc_id = aws_vpc.vpc.id
+#   service_name = "com.amazonaws.eu-west-2.sts"
+#   vpc_endpoint_type = "Interface"
 
-  subnet_ids = [
-    aws_subnet.subnet_az1.id,
-    aws_subnet.subnet_az2.id,
-    aws_subnet.subnet_az3.id
-  ]
+#   subnet_ids = [
+#     aws_subnet.subnet_az1.id,
+#     aws_subnet.subnet_az2.id,
+#     aws_subnet.subnet_az3.id
+#   ]
 
-  security_group_ids = [
-    aws_security_group.allow_vpc_https_traffic.id
-  ]
+#   security_group_ids = [
+#     aws_security_group.allow_vpc_https_traffic.id
+#   ]
 
-  private_dns_enabled = true
+#   private_dns_enabled = true
 
-  tags = {
-    Name = "STS VPC Endpoint"
-    Project = "Catalyst"
-  }
-}
+#   tags = {
+#     Name = "STS VPC Endpoint"
+#     Project = "Catalyst"
+#   }
+# }
 
-resource "aws_vpc_endpoint" "execute-api_vpc_endpoint" {
-  vpc_id = aws_vpc.vpc.id
-  service_name = "com.amazonaws.eu-west-2.execute-api"
-  vpc_endpoint_type = "Interface"
+# resource "aws_vpc_endpoint" "execute-api_vpc_endpoint" {
+#   vpc_id = aws_vpc.vpc.id
+#   service_name = "com.amazonaws.eu-west-2.execute-api"
+#   vpc_endpoint_type = "Interface"
 
-  subnet_ids = [
-    aws_subnet.subnet_az1.id,
-    aws_subnet.subnet_az2.id,
-    aws_subnet.subnet_az3.id
-  ]
+#   subnet_ids = [
+#     aws_subnet.subnet_az1.id,
+#     aws_subnet.subnet_az2.id,
+#     aws_subnet.subnet_az3.id
+#   ]
 
-  security_group_ids = [
-    aws_security_group.allow_vpc_https_traffic.id
-  ]
+#   security_group_ids = [
+#     aws_security_group.allow_vpc_https_traffic.id
+#   ]
 
-  private_dns_enabled = true
+#   private_dns_enabled = true
 
-  tags = {
-    Name = "Execute API VPC Endpoint"
-    Project = "Catalyst"
-  }
-}
+#   tags = {
+#     Name = "Execute API VPC Endpoint"
+#     Project = "Catalyst"
+#   }
+# }
 
-resource "aws_vpc_endpoint" "secretsmanager_vpc_endpoint" {
-  vpc_id = aws_vpc.vpc.id
-  service_name = "com.amazonaws.eu-west-2.secretsmanager"
-  vpc_endpoint_type = "Interface"
+# resource "aws_vpc_endpoint" "secretsmanager_vpc_endpoint" {
+#   vpc_id = aws_vpc.vpc.id
+#   service_name = "com.amazonaws.eu-west-2.secretsmanager"
+#   vpc_endpoint_type = "Interface"
 
-  subnet_ids = [
-    aws_subnet.subnet_az1.id,
-    aws_subnet.subnet_az2.id,
-    aws_subnet.subnet_az3.id
-  ]
+#   subnet_ids = [
+#     aws_subnet.subnet_az1.id,
+#     aws_subnet.subnet_az2.id,
+#     aws_subnet.subnet_az3.id
+#   ]
 
-  security_group_ids = [
-    aws_security_group.allow_vpc_https_traffic.id
-  ]
+#   security_group_ids = [
+#     aws_security_group.allow_vpc_https_traffic.id
+#   ]
 
-  private_dns_enabled = true
+#   private_dns_enabled = true
 
-  tags = {
-    Name = "Secrets Manager VPC Endpoint"
-    Project = "Catalyst"
-  }
-}
+#   tags = {
+#     Name = "Secrets Manager VPC Endpoint"
+#     Project = "Catalyst"
+#   }
+# }
