d73bd77285996cbd88e1f16f842388508c9ce6c6
Graham King <animandosolutions@gmail.com>
2023-03-15 12:18:17 +0000

    Remove route tables

--- routeTables.tf
+++ routeTables.tf
@@ -1,86 +1,86 @@
-resource "aws_route_table" "igw_route_table" {
-  vpc_id = aws_vpc.vpc.id
+# resource "aws_route_table" "igw_route_table" {
+#   vpc_id = aws_vpc.vpc.id
 
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
-  }
+#   route {
+#     cidr_block = "0.0.0.0/0"
+#     gateway_id = aws_internet_gateway.igw.id
+#   }
 
-  tags = {
-    Name = "Internet Gateway Route Table"
-    Project = "Catalyst"
-  }
-}
+#   tags = {
+#     Name = "Internet Gateway Route Table"
+#     Project = "Catalyst"
+#   }
+# }
 
-resource "aws_route_table_association" "public_route_table_az1" {
-  subnet_id = aws_subnet.public_subnet_az1.id
-  route_table_id = aws_route_table.igw_route_table.id
-}
+# resource "aws_route_table_association" "public_route_table_az1" {
+#   subnet_id = aws_subnet.public_subnet_az1.id
+#   route_table_id = aws_route_table.igw_route_table.id
+# }
 
-resource "aws_route_table_association" "public_route_table_az2" {
-  subnet_id = aws_subnet.public_subnet_az2.id
-  route_table_id = aws_route_table.igw_route_table.id
-}
+# resource "aws_route_table_association" "public_route_table_az2" {
+#   subnet_id = aws_subnet.public_subnet_az2.id
+#   route_table_id = aws_route_table.igw_route_table.id
+# }
 
-resource "aws_route_table_association" "public_route_table_az3" {
-  subnet_id = aws_subnet.public_subnet_az3.id
-  route_table_id = aws_route_table.igw_route_table.id
-}
+# resource "aws_route_table_association" "public_route_table_az3" {
+#   subnet_id = aws_subnet.public_subnet_az3.id
+#   route_table_id = aws_route_table.igw_route_table.id
+# }
 
 
-resource "aws_route_table" "private_route_table_az1" {
-  vpc_id = aws_vpc.vpc.id
+# resource "aws_route_table" "private_route_table_az1" {
+#   vpc_id = aws_vpc.vpc.id
 
-  route {
-    cidr_block = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.public_nat_gateway_az1.id
-  }
+#   route {
+#     cidr_block = "0.0.0.0/0"
+#     nat_gateway_id = aws_nat_gateway.public_nat_gateway_az1.id
+#   }
 
-  tags = {
-    Name = "Private Route Table AZ1"
-    Project = "Catalyst"
-  }
-}
+#   tags = {
+#     Name = "Private Route Table AZ1"
+#     Project = "Catalyst"
+#   }
+# }
 
-resource "aws_route_table" "private_route_table_az2" {
-  vpc_id = aws_vpc.vpc.id
+# resource "aws_route_table" "private_route_table_az2" {
+#   vpc_id = aws_vpc.vpc.id
 
-  route {
-    cidr_block = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.public_nat_gateway_az2.id
-  }
+#   route {
+#     cidr_block = "0.0.0.0/0"
+#     nat_gateway_id = aws_nat_gateway.public_nat_gateway_az2.id
+#   }
 
-  tags = {
-    Name = "Private Route Table AZ2"
-    Project = "Catalyst"
-  }
-}
+#   tags = {
+#     Name = "Private Route Table AZ2"
+#     Project = "Catalyst"
+#   }
+# }
 
-resource "aws_route_table" "private_route_table_az3" {
-  vpc_id = aws_vpc.vpc.id
+# resource "aws_route_table" "private_route_table_az3" {
+#   vpc_id = aws_vpc.vpc.id
 
-  route {
-    cidr_block = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.public_nat_gateway_az3.id
-  }
+#   route {
+#     cidr_block = "0.0.0.0/0"
+#     nat_gateway_id = aws_nat_gateway.public_nat_gateway_az3.id
+#   }
 
-  tags = {
-    Name = "Private Route Table AZ3"
-    Project = "Catalyst"
-  }
-}
+#   tags = {
+#     Name = "Private Route Table AZ3"
+#     Project = "Catalyst"
+#   }
+# }
 
-resource "aws_route_table_association" "private_route_table_assoc_az1" {
-  subnet_id = aws_subnet.subnet_az1.id
-  route_table_id = aws_route_table.private_route_table_az1.id
-}
+# resource "aws_route_table_association" "private_route_table_assoc_az1" {
+#   subnet_id = aws_subnet.subnet_az1.id
+#   route_table_id = aws_route_table.private_route_table_az1.id
+# }
 
-resource "aws_route_table_association" "private_route_table_assoc_az2" {
-  subnet_id = aws_subnet.subnet_az2.id
-  route_table_id = aws_route_table.private_route_table_az2.id
-}
+# resource "aws_route_table_association" "private_route_table_assoc_az2" {
+#   subnet_id = aws_subnet.subnet_az2.id
+#   route_table_id = aws_route_table.private_route_table_az2.id
+# }
 
-resource "aws_route_table_association" "private_route_table_assoc_az3" {
-  subnet_id = aws_subnet.subnet_az3.id
-  route_table_id = aws_route_table.private_route_table_az3.id
-}
+# resource "aws_route_table_association" "private_route_table_assoc_az3" {
+#   subnet_id = aws_subnet.subnet_az3.id
+#   route_table_id = aws_route_table.private_route_table_az3.id
+# }
--- subnets.tf
+++ subnets.tf
@@ -42,24 +42,24 @@ resource "aws_subnet" "public_subnet_az1" {
   }
 }
 
-resource "aws_subnet" "public_subnet_az2" {
-  availability_zone = data.aws_availability_zones.azs.names[1]
-  cidr_block        = "10.0.21.0/24"
-  vpc_id            = aws_vpc.vpc.id
+# resource "aws_subnet" "public_subnet_az2" {
+#   availability_zone = data.aws_availability_zones.azs.names[1]
+#   cidr_block        = "10.0.21.0/24"
+#   vpc_id            = aws_vpc.vpc.id
 
-  tags = {
-    Name = "Public Subnet AZ2"
-    Project = "Catalyst"
-  }
-}
+#   tags = {
+#     Name = "Public Subnet AZ2"
+#     Project = "Catalyst"
+#   }
+# }
 
-resource "aws_subnet" "public_subnet_az3" {
-  availability_zone = data.aws_availability_zones.azs.names[2]
-  cidr_block        = "10.0.31.0/24"
-  vpc_id            = aws_vpc.vpc.id
+# resource "aws_subnet" "public_subnet_az3" {
+#   availability_zone = data.aws_availability_zones.azs.names[2]
+#   cidr_block        = "10.0.31.0/24"
+#   vpc_id            = aws_vpc.vpc.id
 
-  tags = {
-    Name = "Public Subnet AZ3"
-    Project = "Catalyst"
-  }
-}
+#   tags = {
+#     Name = "Public Subnet AZ3"
+#     Project = "Catalyst"
+#   }
+# }
